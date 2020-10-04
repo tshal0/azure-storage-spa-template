f48fc134b9810ceca0190c765c56116b91470402
Thomas Shallenberger <twshal@gmail.com>
2020-10-03 23:34:15 -0500

    Removed some console logs

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 const spa = __dirname + '/spa/dist';
-console.log(__dirname)
+console.log(__dirname);
 // serve static assets normally
 app.use(express.static(spa));
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response) {
-  console.log(__dirname)
-  console.log(spa)
   response.sendFile(path.resolve(spa, 'index.html'));
 });
 
